d14dbe2c4cc719b51b919df7e6cae1dd4729a6d2
Raphael Silva <sr.silvaraphael@gmail.com>
2019-12-30 16:35:42 +0100

    Extracts invalid store URL creating into a helper factory method.

--- Tests/FeedStoreChallengeTests.swift
+++ Tests/FeedStoreChallengeTests.swift
@@ -187,6 +187,10 @@ class FeedStoreChallengeTests: XCTestCase, FeedStoreSpecs {
     private func writeInvalidData(to storeURL: URL) {
         try! "invalid data".write(to: storeURL, atomically: false, encoding: .utf8)
     }
+    
+    private func invalidStoreURL() -> URL {
+        return URL(string: "invalid://store-url")!
+    }
 }
 
 extension FeedStoreChallengeTests: FailableRetrieveFeedStoreSpecs {
@@ -214,15 +218,15 @@ extension FeedStoreChallengeTests: FailableRetrieveFeedStoreSpecs {
 extension FeedStoreChallengeTests: FailableInsertFeedStoreSpecs {
 
     func test_insert_deliversErrorOnInsertionError() {
-        let invalidStoreURL = URL(string: "invalid://store-url")
-        let sut = makeSUT(storeURL: invalidStoreURL)
+        let invalidURL = invalidStoreURL()
+        let sut = makeSUT(storeURL: invalidURL)
 
         assertThatInsertDeliversErrorOnInsertionError(on: sut)
     }
 
     func test_insert_hasNoSideEffectsOnInsertionError() {
-        let invalidStoreURL = URL(string: "invalid://store-url")
-        let sut = makeSUT(storeURL: invalidStoreURL)
+        let invalidURL = invalidStoreURL()
+        let sut = makeSUT(storeURL: invalidURL)
 
         assertThatInsertHasNoSideEffectsOnInsertionError(on: sut)
     }
