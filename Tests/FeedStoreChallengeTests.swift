50123d499d16c0a87492eebc3cf6fb003131575b
Raphael Silva <sr.silvaraphael@gmail.com>
2020-03-21 22:18:31 +0100

    FeedStoreChallengeTests: deleting has no side effects on deletion error.

--- Tests/FeedStoreChallengeTests.swift
+++ Tests/FeedStoreChallengeTests.swift
@@ -166,9 +166,9 @@ extension FeedStoreChallengeTests: FailableDeleteFeedStoreSpecs {
 	}
 
 	func test_delete_hasNoSideEffectsOnDeletionError() {
-//		let sut = makeSUT()
-//
-//		assertThatDeleteHasNoSideEffectsOnDeletionError(on: sut)
+		let sut = FeedStoreStub(failure: [.delete])
+
+		assertThatDeleteHasNoSideEffectsOnDeletionError(on: sut)
 	}
 
 }
